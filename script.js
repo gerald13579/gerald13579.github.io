ea5d2bb76bdf2be09d3db1e15281fd21906e6f84
gerald13579 <geraldtakalamingan@gmail.com>
2024-04-19 20:50:03 +0800

    commit keempat

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var fullImgBox = document.getElementById("fullImgBox");
 var fullImg = document.getElementById("fullImg");
 
+let menuList = document.getElementById("menuList")
+menuList.style.maxHeight = "0px";
+
 function openFullImg(pic){
     fullImgBox.style.display = "flex";
     fullImg.src = pic;
@@ -8,4 +11,12 @@ function openFullImg(pic){
 
 function closeFullImg(){
     fullImgBox.style.display = "none";
+}
+
+function toggleMenu(){
+    if(menuList.style.maxHeight == "0px"){
+        menuList.style.maxHeight = "300px";
+    } else {
+        menuList.style.maxHeight = "0px";
+    }
 }
